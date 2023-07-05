c5cdde0d8b330541ce509c835db1ccc7d88f5b53
Gustavo Taufembach Bett <gustavotaufembachbtt@gmail.com>
2023-07-04 21:38:37 -0300

    refazendo o back em java

---
+++ back-java17-spring2.7/habits/src/main/java/com/rocket/habits/HabitsApplication.java
@@ -0,0 +1,13 @@
+package com.rocket.habits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitsApplication.class, args);
+	}
+
+}
---
+++ back-java17-spring2.7/habits/src/test/java/com/rocket/habits/HabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocket.habits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
